d2f45991ae6c987b621fe6e590cebc1e6bf60ef3
Martin Garton <martin.garton@ft.com>
2016-01-05 18:00:30 +0000

    use github.com/Financial-Times/neo-cypher-runner-go

--- batch_writer.go
+++ batch_writer.go
@@ -1,95 +1,15 @@
 package neoutil
 
 import (
+	"github.com/Financial-Times/neo-cypher-runner-go"
 	"github.com/jmcvetta/neoism"
-	"log"
 	"time"
 )
 
+func NewBatchWriter(db *neoism.Database, batchSize int) CypherRunner {
+	return neocypherrunner.NewBatchCypherRunner(db, batchSize, time.Millisecond*20)
+}
+
 type CypherRunner interface {
 	CypherBatch(queries []*neoism.CypherQuery) error
-}
-
-type batchWriter struct {
-	db         *neoism.Database
-	writeQueue chan writeEntry
-	batchSize  int
-}
-
-type writeEntry struct {
-	queries []*neoism.CypherQuery
-	err     chan error
-}
-
-// NewSafeWriter provides a new batch writer which will batch writes internally
-// without risking data loss.
-func NewBatchWriter(db *neoism.Database, batchSize int) CypherRunner {
-	sw := &batchWriter{db, make(chan writeEntry, batchSize), batchSize}
-	go sw.writeLoop()
-	return sw
-}
-
-func (sw *batchWriter) CypherBatch(queries []*neoism.CypherQuery) error {
-	we := writeEntry{queries, make(chan error)}
-	sw.writeQueue <- we
-	return <-we.err
-}
-
-func (sw *batchWriter) writeLoop() {
-
-	var qs []writeEntry
-
-	timer := time.NewTimer(1 * time.Second)
-
-	defer log.Println("write loop exited")
-	for {
-		select {
-		case writeEntry, ok := <-sw.writeQueue:
-			if !ok {
-				return
-			}
-			qs = append(qs, writeEntry)
-
-			if len(qs) < sw.batchSize {
-				timer.Reset(20 * time.Millisecond)
-				continue
-			}
-		case <-timer.C:
-		}
-
-		if len(qs) > 0 {
-			timer.Stop()
-			log.Printf("writing batch of %d\n", len(qs))
-			var batched []*neoism.CypherQuery
-			var errChs []chan error
-			for _, we := range qs {
-				batched = append(batched, we.queries...)
-				errChs = append(errChs, we.err)
-			}
-
-			err := sw.db.CypherBatch(batched)
-
-			/*  not batched
-			var err error
-			for _, q := range batched {
-				if err == nil {
-					err = sw.db.Cypher(q)
-				}
-			}
-			*/
-
-			// transactional
-			//tx, err := sw.db.Begin(batched)
-			//if err == nil {
-			//		err = tx.Commit()
-			//	}
-
-			for _, errCh := range errChs {
-				errCh <- err
-			}
-
-			log.Printf("wrote batch of %d\n", len(qs))
-			qs = qs[0:0]
-		}
-	}
 }
